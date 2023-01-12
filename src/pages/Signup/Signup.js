2d0593c52b9d3011ead8e6f9a97fccf41938b174
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-01-12 11:52:56 +0530

    Add "signup" feature

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,16 +1,52 @@
 import Button from "@restart/ui/esm/Button";
 import React, { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
+import { gql, useMutation } from "@apollo/client";
+
+const SIGNUP = gql`
+  mutation SignUp($credentials: CredentialsInput!, $name: String!, $bio: String!) {
+  signup(credentials: $credentials, name: $name, bio: $bio) {
+    userErrors {
+      message
+    }
+    token
+  }
+}
+`;
 
 export default function Signup() {
+  const [signup, { data }] = useMutation(SIGNUP);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [bio, setBio] = useState("");
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    signup({
+      variables: {
+        credentials: {
+          email,
+          password
+        },
+        name,
+        bio
+      }
+    });
+  };
 
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      if (data.signup.userErrors.length) {
+        setError(data.signup.userErrors[0].message);
+      }
+      if (data.signup.token) {
+        localStorage.setItem("token", data.signup.token);
+      }
+    }
+  }, [data]);
 
   return (
     <div>
