21eb7377c011affd282a5927ee7b50eed9606df7
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-01-15 20:43:36 +0530

    Add home page

---
+++ src/pages/Home/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+
+
+    return (
+        <>
+            <div className="d-flex flex-column justify-content-center align-items-center min-vh-100" style={{ "background-color": "#ced4da" }}>
+                <p className="fs-3 fw-semibold">Udemy GraphQL course project</p>
+            </div>
+
+        </>
+    );
+}
