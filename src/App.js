dc1bd849b8c35afe6b07a7d126f834c46d9cccc9
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-01-17 18:52:50 +0530

    Add routes and change css

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Profile from "./pages/Profile/Profile";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./pages/Signup/Signup";
 import Signin from "./pages/Signin/Signin";
+import Home from "./pages/Home/home";
+import NavigationBar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <Switch>
+        <Route strict exact path="/" component={Home} />
         <Route strict exact path="/posts" component={Posts} />
         <Route strict exact path="/signup" component={Signup} />
         <Route strict exact path="/signin" component={Signin} />
