d86b41342af217226cbf3b54ae11245b3dd2fc83
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-01-21 21:09:49 +0530

    bug fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,11 +5,11 @@ import Navbar from 'react-bootstrap/Navbar';
 function NavigationBar() {
     return (
         <>
-            <Navbar bg="dark" variant="dark" style={{"position": "absolute", "width": "100vw"}}>
+            <Navbar bg="dark" variant="dark" style={{"position": "absolute", "width": "100vw", "top": "0"}}>
                 <Container>
                     <Navbar.Brand href="#home">GraphQL Demo</Navbar.Brand>
                     <Nav className="">
-                        <Nav.Link href="/" style={{"color": "#fff"}} >Home</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/posts">Posts</Nav.Link>
                         <Nav.Link href="/signin">Sign In</Nav.Link>
                         <Nav.Link href="#pricing">Sign Up</Nav.Link>
