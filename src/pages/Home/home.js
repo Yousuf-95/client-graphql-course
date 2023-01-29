0fc7400961b84e57514001bc09ac5a35b3a4f9b9
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-01-29 18:47:07 +0530

    bug fix

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,7 +5,7 @@ export default function Home() {
 
     return (
         <>
-            <div className="d-flex flex-column justify-content-center align-items-center min-vh-100" style={{ "background-color": "#ced4da" }}>
+            <div className="d-flex flex-column justify-content-center align-items-center min-vh-100" style={{ "backgroundColor": "#ced4da" }}>
                 <p className="fs-3 fw-semibold">Udemy GraphQL course project</p>
             </div>
 
