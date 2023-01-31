8965063752f52aa5621b0b6d04ddd3382b703dc5
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-01-31 23:04:04 +0530

    Conditional rendering of sign in/sign up links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { AuthContext } from '../../context/authContext';
 
 function NavigationBar() {
+    const {authState} = useContext(AuthContext);
+
     return (
         <>
             <Navbar bg="dark" variant="dark" style={{"position": "absolute", "width": "100vw", "top": "0"}}>
@@ -11,8 +15,9 @@ function NavigationBar() {
                     <Nav className="">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/posts">Posts</Nav.Link>
-                        <Nav.Link href="/signin">Sign In</Nav.Link>
-                        <Nav.Link href="#pricing">Sign Up</Nav.Link>
+                        {authState.isAuthenticated === 'false' && <Nav.Link href="/signin">Sign In</Nav.Link>}
+                        {authState.isAuthenticated === 'false' && <Nav.Link href="#pricing">Sign Up</Nav.Link>}
+                        
                     </Nav>
                 </Container>
             </Navbar>
